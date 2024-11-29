8d687080cd392f470fe362abd5556826f5d48b51
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2024-11-29 16:52:59 -0600

    Practica de plantilla

--- holamundo/urls.py
+++ holamundo/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("herencia/", views.herencia, name="herencia"),
-    path("ejemplo/", views.ejemplo, name="ejemplo"),
-    path("otra/", views.otra, name="otra"),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("portfolio/", views.portfolio, name="portfolio")
 ]
